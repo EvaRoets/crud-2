8ec23073d4c4e668fc428e779af99c16bc1b4426
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-19 12:30:19 +0200

    Add entries to database when btn clicked

--- process.php
+++ process.php
@@ -1,3 +1,14 @@
 <?php
-
+//establish connection
 $mysql = new mysql('localhost', 'root', 'root', 'crud-2') or die(mysqli_error($mysql));
+
+//check if addBook button is clicked
+if (isset($_POST['addBook'])){
+   //save entries in variables
+    $bookTitle = $_POST['bookTitle'];
+    $author = $_POST['author'];
+
+    //insert entries in database or display syntax error
+    $insert = "INSERT INTO data (book title, author) VALUES ('$bookTitle', '$author')";
+    $mysql->query($insert) or die($mysql-error);
+}
