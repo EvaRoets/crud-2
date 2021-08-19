552523a87efc729809f790764f87dc51bf93a9f0
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-19 18:10:45 +0200

    Add functionality buttons

--- index.php
+++ index.php
@@ -10,62 +10,60 @@
     <title>Document</title>
 </head>
 <body>
-    <?php require_once "process.php"; ?>
-    <div class="container">
-        <!-- display existing records-->
-        <?php
-        $mysqli = new mysqli('localhost', 'root', '', 'crud-2') or die(mysqli_error($mysqli));
-        $result = $mysqli->query("SELECT * FROM data") or die(mysqli_error($mysqli));
 
-        //    var_dump("<pre>");
-        //    var_dump($result->fetch_assoc());
-        //    var_dump($result->fetch_assoc());
-        //    var_dump($result->fetch_assoc());
-        //    var_dump("</pre>");
-        ?>
-        <!-- add table -->
-        <div class="row justify-content-center">
-            <table class="table">
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th colspan="2">Actions</th>
-                </tr>
-                </thead>
-        <?php
+<?php require_once "process.php"; ?>
+
+<div class="container">
+    <!-- display existing records-->
+    <?php
+    $mysqli = new mysqli('localhost', 'root', '', 'crud-2') or die(mysqli_error($mysqli));
+    $result = $mysqli->query("SELECT * FROM data") or die(mysqli_error($mysqli));
+    ?>
+
+    <!-- add table -->
+    <div class="row justify-content-center">
+        <table class="table">
+            <thead>
+            <tr>
+                <th>Title</th>
+                <th>Author</th>
+                <th colspan="2">Action</th>
+            </tr>
+            </thead>
+
+            <?php
             while ($row = $result->fetch_assoc()): ?>
                 <tr>
-                    <td><?= $row['title'] ?></td>
-                    <td><?= $row['author'] ?></td>
+                    <td><?= $row['title']; ?></td>
+                    <td><?= $row['author']; ?></td>
                     <td>
-                        <a href="index.php?edit"=<?= $row['id']; ?>
-                            class="btn btn-info">Edit</a>
-                        <a href="process.phpdelete"=<?= $row['id']; ?>
+                        <a href="index.php?edit=<?= $row['id']; ?>"
+                        class="btn btn-info">Edit</a>
+                        <a href="process.php?delete=<?= $row['id']; ?>"
                         class="btn btn-danger">Delete</a>
                     </td>
                 </tr>
             <?php endwhile; ?>
-            </table>
-        </div>
-
-        <div class="col-lg-4 col-md-4 col-sm-4 container justify-content-center">
-            <form action="process.php" method="POST">
-                <div class="form-group">
-                    <label for="title">Book Title</label>
-                    <input type="text" name="title" class="form-control" placeholder="Enter the book title">
-                </div>
-
-                <div class="form-group">
-                    <label for="author">Author</label>
-                    <input type="text" name="author" class="form-control" placeholder="Enter the author">
-                </div>
-
-                <div class="form-group text-center">
-                    <button type="submit" name="addBook" class="btn btn-primary">Add book</button>
-                </div>
-            </form>
-        </div>
+        </table>
     </div>
+
+    <div class="col-lg-4 col-md-4 col-sm-4 container justify-content-center">
+        <form action="process.php" method="POST">
+            <div class="form-group">
+                <label for="title">Book Title</label>
+                <input type="text" name="title" class="form-control" placeholder="Enter the book title">
+            </div>
+
+            <div class="form-group">
+                <label for="author">Author</label>
+                <input type="text" name="author" class="form-control" placeholder="Enter the author">
+            </div>
+
+            <div class="form-group text-center">
+                <button type="submit" name="addBook" class="btn btn-primary">Add book</button>
+            </div>
+        </form>
+    </div>
+</div>
 </body>
 </html>
--- process.php
+++ process.php
@@ -3,12 +3,20 @@
 $mysqli = new mysqli('localhost', 'root', '', 'crud-2') or die(mysqli_error($mysql));
 
 //check if addBook button is clicked
-if (isset($_POST['addBook'])){
-   //save entries in variables
+if (isset($_POST['addBook'])) {
+    //save entries in variables
     $title = $_POST['title'];
     $author = $_POST['author'];
 
     //insert entries in database or display syntax error
     $insert = "INSERT INTO data (title , author) VALUES ('$title', '$author')";
     $mysqli->query($insert) or die($mysqli->error);
+}
+
+//check if delete button is clicked with get because an url is passed
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $delete = "DELETE FROM data WHERE id=$id";
+    $mysqli->query($delete) or die($mysqli->error);
+
 }
