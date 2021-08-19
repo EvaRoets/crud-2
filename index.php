7262dc64f5425e6ac64eb832323b53b03440db6e
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-08-19 09:52:22 +0200

    Add basic bootstrap styling

--- index.php
+++ index.php
@@ -9,12 +9,22 @@
     <title>Document</title>
 </head>
 <body>
-<form action="" method="POST">
-    <label>Book Title</label>
-    <input type="text" name="bookTitle" value="Enter the book title" required>
-    <label>Author</label>
-    <input type="text" name="author" value="Enter the author">
-    <button type="submit" name="addBook">Add book</button>
-</form>
+    <div class="col-lg-4 col-md-4 col-sm-4 container justify-content-center">
+        <form action="" method="POST">
+            <div class="form-group">
+                <label for="bookTitle">Book Title</label>
+                <input type="text" name="bookTitle" class="form-control" value="Enter the book title" required>
+            </div>
+
+            <div class="form-group">
+                <label for="author">Author</label>
+                <input type="text" name="author" class="form-control" value="Enter the author">
+            </div>
+
+            <div class="form-group text-center">
+                <button type="submit" name="addBook" class="btn btn-primary">Add book</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
