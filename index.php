220bc019455c873c04ed7a0d35023b0b69efae60
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-02 23:05:09 +0200

    Add styling

--- index.php
+++ index.php
@@ -10,18 +10,37 @@
     <title>Your list of books</title>
 </head>
 <body>
-    <?php require_once "process.php"; ?>
-
-    <!--add action message-->
-    <?php
-    if (isset($_SESSION["message"])): ?>
-        <div class="alert alert-<?=$_SESSION["msg_type"]?> ">
-            <?php
-            echo $_SESSION["message"];
-            unset($_SESSION["message"]);
-            ?>
+<header>
+    <!-- Background image -->
+    <div class="p-5 text-center bg-image sticky-top"
+         style="
+      background-image: url('./images/books.jpg');
+      height: 200px;
+      margin-bottom: 50px;
+    ">
+        <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+            <div class="d-flex justify-content-center align-items-center h-100">
+                <div class="text-white">
+                    <h1 class="mb-3">Your list of books</h1>
+                    <h4 class="mb-3">Keep track of all the books you want to read </h4>
+                </div>
+            </div>
         </div>
-    <?php endif; ?>
+    </div>
+</header>
+
+<?php require_once "process.php"; ?>
+
+<!--add action message-->
+<?php
+if (isset($_SESSION["message"])): ?>
+    <div class="alert alert-<?= $_SESSION["msg_type"] ?> ">
+        <?php
+        echo $_SESSION["message"];
+        unset($_SESSION["message"]);
+        ?>
+    </div>
+<?php endif; ?>
 
 <!-- display existing records-->
 <div class="container">
@@ -31,24 +50,24 @@
     ?>
 
     <!-- add form-->
-    <div class="col-lg-4 col-md-4 col-sm-4 container justify-content-center">
+    <div class="col-lg-7 col-md-7 col-sm-7 container justify-content-center">
         <form action="process.php" method="POST">
-            <input type="hidden" name="id" value="<?= $id; ?>" >
-            <div class="form-group">
+            <input type="hidden" name="id" value="<?= $id; ?>">
+            <div class="form-group" style="margin:15px;">
                 <label for="title">Book Title</label>
                 <input type="text" name="title" class="form-control"
                        value="<?= $title; ?>"
                        placeholder="Enter the book title">
             </div>
 
-            <div class="form-group">
+            <div class="form-group" style="margin:15px;">
                 <label for="author">Author</label>
                 <input type="text" name="author" class="form-control"
                        value="<?= $author; ?>"
                        placeholder="Enter the author">
             </div>
 
-            <div class="form-group text-center">
+            <div class="form-group text-center" style="margin:15px;">
                 <?php if ($update == true): ?>
                     <button type="submit" name="update" class="btn btn-warning">Update</button>
                 <?php else: ?>
@@ -57,7 +76,6 @@
             </div>
         </form>
     </div>
-
 
     <!-- add table -->
     <div class="row justify-content-center">
@@ -78,14 +96,37 @@
                     <td><?= $row['author']; ?></td>
                     <td>
                         <a href="index.php?edit=<?= $row['id']; ?>"
-                           class="btn btn-info">Edit</a>
+                           class="btn btn-info"
+                           style="margin:10px;">Edit</a>
                         <a href="process.php?delete=<?= $row['id']; ?>"
-                           class="btn btn-danger">Delete</a>
+                           class="btn btn-danger"
+                           style="margin:10px;">Delete</a>
                     </td>
                 </tr>
             <?php endwhile; ?>
+
         </table>
     </div>
 </div>
+<footer>
+    <div class="p-4 text-center bg-image sticky-bottom"
+         style="
+      background-image: url('./images/books.jpg');
+      height: 40px;
+      margin-top: 80px;
+    ">
+        <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);
+            <div class=" d-flex justify-content-center align-items-center h-100
+        ">
+        <div class="text-white">
+            Copyright &copy;
+            <script>document.write(new Date().getFullYear())</script>
+            Eva Roets
+        </div>
+    </div>
+    </div>
+    </div>
+
+</footer>
 </body>
 </html>
